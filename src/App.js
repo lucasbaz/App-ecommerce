008ef3b6cc2cd3095805fe8777ad681176afcec5
lucasbaz <b.lucasgabriel@gmail.com>
2022-05-11 15:05:13 -0300

    details to entrega desafio 1

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ function App() {
   return (
     <div className="App">
       <h1>e-commerce APP v0.1</h1>
+      <div>
+        <p>Entrega de desafio 1 - <strong>creacion del CLI</strong> </p>
+      </div>
     </div>
   );
 }
