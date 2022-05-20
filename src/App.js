f2af4e5a21d174b6ba5d37194ba477aaf16bbc78
lucasbaz <b.lucasgabriel@gmail.com>
2022-05-20 00:07:09 -0300

    add more style to carditem

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import './components/NavBar.css';
 import NavBar from './components/NavBar';
-import Card from './components/Cards/Card';
+import CardItem from './components/Cards/Card';
+import { Container, Grid } from '@mui/material';
+import { Grid4x4 } from '@mui/icons-material';
 
 function App() {
   return (
@@ -11,7 +13,14 @@ function App() {
       <div>
         <p>Entrega de desafio 1 - <strong>creacion del CLI</strong> </p>
       </div>
-      <Card></Card>
+      <Container>
+        <Grid container>
+          <Grid item md={4}>
+            <CardItem></CardItem>
+          </Grid>
+        </Grid>
+      </Container>
+      
     </div>
   );
 }
--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -1,10 +1,19 @@
-
-const Card = () => {
+import { Button } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import { ButtonToolbar } from 'react-bootstrap';
+const CardItem = () => {
     return (
         <div>
-            <p>Hola soy eve</p>
+            <div>
+                <img src="https://d3ugyf2ht6aenh.cloudfront.net/stores/932/712/products/wapsbo11-792913a21ae907f05416077122463096-240-0.jpg" />
+            </div>
+            <p>Titulo</p>
+            <span> price $$ </span> 
+            <Button variant="contained" > details </Button>
         </div>
     )
 }
 
-export default Card
+export default CardItem
